8488b86c86e5c022de6ec6c9b51da77d3cfbe979
Israalulu0 <israalulu0@gmail.com>
2022-09-28 10:59:39 +0300

    Initial commit

---
+++ Q2.py
@@ -0,0 +1,5 @@
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+#tuple can deal with index 20 is in the index 1 and then 1 in the list inside
+print(tuple1[1][1])
+
---
+++ Q3.py
@@ -0,0 +1,21 @@
+#Q3
+tuple = (10, 20, 30, 40)
+
+#first sol
+a=tuple[0]
+b=tuple[1]
+c=tuple[2]
+d=tuple[3]
+print(a)
+print(b)
+print(c)
+print(d)
+
+#second sol
+
+a,b,c,d = tuple
+print(a,b,c,d)
+print(a)
+print(b)
+print(c)
+print(d)
